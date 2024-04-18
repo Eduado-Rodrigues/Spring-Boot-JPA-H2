8063d2bf9d14d95a0a1fe48b7f97f48cc3cbc172
Eduardo Rodrigues Oliveira <159492869+Eduado-Rodrigues@users.noreply.github.com>
2024-04-17 22:18:58 -0300

    Services e exceptions

---
+++ usuario/src/main/java/com/edutec/usuario/services/UserService.java
@@ -0,0 +1,22 @@
+package com.edutec.usuario.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.edutec.usuario.entities.User;
+
+@Service
+public interface UserService {
+    
+    List<User> findAll();
+    
+    Optional<User> findById(Long id);
+    
+    User insert(User user);
+    
+    void delete(Long id);
+    
+    User update(Long id, User user);
+}
---
+++ usuario/src/main/java/com/edutec/usuario/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.edutec.usuario.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
